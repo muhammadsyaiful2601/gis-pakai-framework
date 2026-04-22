facb49132e31376d0799cbe6d115e997f0a76125
Udingamingcd <muhammadsyaiful2601@gmail.com>
2026-04-22 15:37:53 +0700

    dashboard

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'judul' => 'Dashboard',
+        ];
+        return view('v_template', $data);
     }
 }
